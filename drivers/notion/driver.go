459d8d09a429d01e9c3476c9c9ebb9aa4829ac6e
zxw <zeus@tytexchina.com>
2025-08-26 16:51:32 +0800

    测试

--- drivers/notion/driver.go
+++ drivers/notion/driver.go
@@ -468,6 +468,10 @@ func (d *Notion) putSingleFile(ctx context.Context, fileName string, fileSize in
 		return nil, fmt.Errorf("创建Notion页面失败: %v", err)
 	}
 
+	fmt.Printf("页面id：%s", pageID)
+
+	time.Sleep(2000)
+
 	// 上传文件到Notion
 	err = d.notionClient.UploadAndUpdateFilePut(file, pageID, up)
 	if err != nil {
